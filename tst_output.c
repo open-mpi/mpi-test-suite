40463c64fef1c5eb02a09c7815546bd84e65be7c
Christoph Niethammer <niethammer@hlrs.de>
2007-02-22 10:25:16 +0000

    Improved LaTeX and HTML output.
    git-svn-id: https://svn.open-mpi.org/svn/ompi-tests/trunk@923 91b169b6-37d5-0310-8fc8-d193d6184df4

--- tst_output.c
+++ tst_output.c
@@ -30,6 +30,52 @@ Todo:
 #include "tst_output.h"
 #include "mpi_test_suite.h"
 
+/****************************************************************************/
+/**                                                                        **/
+/**                     PROTOTYPES OF LOCAL FUNCTIONS                      **/
+/**                                                                        **/
+/****************************************************************************/
+
+/* Functions to parse output for LaTeX/HTML files 
+ *
+ * Parameters:
+ *      char * src[]    source
+ *      char * dest[]   destination
+ * 
+ * Results:
+ *      Sucess:         Value unequal 0
+ *      Fail:           0
+ */
+/*
+int tst_output_parse_html (char * src[], char * dest[]);
+int tst_output_parse_latex (char * src[], char * dest[]) ;
+*/
+
+/* Function to replace substrings in a string 
+ *
+ * Parameters:
+ *      char * search   string to search for
+ *      char * replace  replacement for searchstring
+ *      char * string   pointer on string to search through
+ *      char * result[] place to store pointer of the result string
+ *
+ * Results:
+ *      Success:        number of replacements
+ */
+int str_replace (char * search, char * replace, char * string, char * result[]);
+
+
+/* Function to mask special chars for latex in a string
+ *
+ * Parameters:
+ *      char * string   string to escape
+ *      char * result[] place to store pointer of the result string
+ * Result:
+ *      Success:        Number of replacements
+ *      Fail:           -1
+ */
+int latex_special_chars (char * string, char * result[]);
+
 
 /****************************************************************************/
 /**                                                                        **/
@@ -217,18 +263,53 @@ int tst_output_printf (tst_output_stream * output,
   if ((output->isopen == 1) && (output->rank == tst_output_global_rank) && (error_level <= output->level)) {
     va_start(arglist, format);
     switch (output->type) {
-      case TST_OUTPUT_TYPE_HTML:
-        fprintf (output->streamptr, "<P>\n");
-        count = vfprintf (output->streamptr, format, arglist);
-        fprintf (output->streamptr, "</P>\n");
+      case TST_OUTPUT_TYPE_HTML: {
+        char * msg;
+        char * tmpstrptr;
+        
+        msg = malloc (sizeof(char) * 2048);
+        count = vsnprintf (msg, 2047, format, arglist);
+        /* Output was truncated */
+        if (count >= 2047)
+          return -1;
+        str_replace ("rank", "<B>rank</B>$", msg, &tmpstrptr);
+        free (msg);
+        str_replace ("Rank", "<B>Rank</B>", tmpstrptr, &msg);
+        free (tmpstrptr);
+        str_replace ("ERROR", "<B style=\"color:red;\">ERROR</B>", msg, &tmpstrptr);
+        free (msg);
+        str_replace ("Error", "<B style=\"color:red;\">Error</B>", tmpstrptr, &msg);
+        free (tmpstrptr);
+        count = fprintf (output->streamptr, "<P>\n%s</P>\n", msg);
         fflush (output->streamptr);
         break;
-      case TST_OUTPUT_TYPE_LATEX:
-        fprintf (output->streamptr, "\\begin{verbatim}\n");
-        count = vfprintf (output->streamptr, format, arglist);
-        fprintf (output->streamptr, "\\end{verbatim}\n");
+        }
+      case TST_OUTPUT_TYPE_LATEX: {
+        char * msg;
+        char * tmpstrptr;
+        
+        msg = malloc (sizeof(char) * 2048);
+        count = vsnprintf (msg, 2047, format, arglist);
+        /* Output was truncated */
+        if (count >= 2047)
+          return -1;
+        latex_special_chars (msg, &tmpstrptr);
+        str_replace ("<", "$<$", tmpstrptr, &msg);
+        free (tmpstrptr);
+        str_replace (">", "$>$", msg, &tmpstrptr);
+        free (msg);
+        str_replace ("Rank", "\\textbf{Rank}", tmpstrptr, &msg);
+        free (tmpstrptr);
+        str_replace ("ERROR", "\\textbf{ERROR}", msg, &tmpstrptr);
+        free (msg);
+        str_replace ("Error", "\\textbf{Error}", tmpstrptr, &msg);
+        free (tmpstrptr);
+
+        count = fprintf (output->streamptr, "%s", msg);
+        fprintf (output->streamptr, "\n\n");
         fflush (output->streamptr);
         break;
+        }
       default:
         count = vfprintf (output->streamptr, format, arglist);
         fflush (output->streamptr);
@@ -243,3 +324,133 @@ int tst_output_printf (tst_output_stream * output,
   return count;
 }
 
+/****************************************************************************/
+/**                                                                        **/
+/**                     LOCAL FUNCTIONS                                    **/
+/**                                                                        **/
+/****************************************************************************/
+
+int latex_special_chars (char * string, char * result[])
+{
+  int count = 0;
+  int newlength = 0;
+  char * strptr = NULL;
+  char * resultptr;
+
+  /* calculate number of special chars */
+  strptr = string;
+  while (*strptr != '\0'){
+    switch (*strptr) {
+      case '\\':
+      case '&':
+      case '$':
+      case '%':
+      case '#':
+      case '_':
+      case '{':
+      case '}':
+        count++;
+      break;
+    }
+    strptr++;
+  }
+  /* allocate memory for the result */
+  newlength = strlen (string) + count;
+  resultptr = malloc(sizeof (char) * (newlength + 1));
+  memset (resultptr, 0, newlength + 1);
+  *result = resultptr;
+  /* do special char masking */
+  strptr = string;
+  while (*strptr != '\0'){
+    switch (*strptr) {
+      case '\\':
+      case '&':
+      case '$':
+      case '%':
+      case '#':
+      case '_':
+      case '{':
+      case '}':
+        *resultptr = '\\';
+        resultptr++;
+        *resultptr = *strptr;
+        resultptr++;
+      break;
+      default:
+        *resultptr = *strptr;
+        resultptr++;
+      break;
+    }
+    strptr++;
+  }
+  *resultptr = '\0';
+
+  return count;
+}
+
+int str_replace (char * search, char * replace, char * string, char * result[])
+{
+  int count = 0, replace_count = 0;
+  int oldlength = 0;
+  int newlength = 0 ;
+  int searchlength = 0;
+  int n;
+  char * strptr = NULL;
+  char * oldstrptr = NULL;
+
+  oldlength = strlen (string);
+  searchlength = strlen (search);
+
+  /* 
+   * Calculate number of neccessary replacements 
+   */
+  for (strptr = string; strptr; strptr++)
+  {
+    strptr = strstr (strptr, search);
+    if (strptr == NULL)
+      break;
+    count++; 
+  }
+  /* 
+   * Calculate new neccessary stinglength and allocate the neccessary memory
+   */
+  newlength = oldlength + count * (strlen(replace) - strlen(search));
+  *result = (char *) malloc ( (newlength + 1) * sizeof (char) );
+  memset (*result, 0, newlength + 1);
+
+  /*
+   * Generate the new String
+   */
+  for (strptr = string; ; )
+  {
+    oldstrptr = strptr;
+    strptr = strstr (strptr, search);
+    /* all replacements done? */
+    if (strptr == NULL) {
+      strcat (*result, oldstrptr);
+      break;
+    }
+    /* Append all up to place of discovery */
+    n = strptr - oldstrptr;
+    strncat (*result, oldstrptr, n);
+    /* Append replace string and move forward in the string */
+    strcat (*result, replace);
+    strptr += searchlength;
+    replace_count++; 
+  }
+
+  /*
+   * Safty check 
+   */
+  if (count != replace_count) {
+    fprintf (stderr, "Error: count (%d) != replace_count (%d)!\n", count, replace_count);
+    exit (-1);
+  }
+  return count;
+}
+
+/****************************************************************************/
+/**                                                                        **/
+/**                     EOF                                                **/
+/**                                                                        **/
+/****************************************************************************/
