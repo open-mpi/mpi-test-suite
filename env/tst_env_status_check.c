a43b0b0c6e6a2e6520f527d0cee1f43625d97cbf
Rainer Keller <rainer.keller@hlrs.de>
2010-02-17 23:12:16 +0000

    - Multiple tests for empty/null MPI_Status and request after    persistent communication...
    git-svn-id: https://svn.open-mpi.org/svn/ompi-tests/trunk@1766 91b169b6-37d5-0310-8fc8-d193d6184df4

--- env/tst_env_status_check.c
+++ env/tst_env_status_check.c
@@ -18,8 +18,8 @@
 #define LOCAL_CHECK(func_string,var,op,expected) \
   if (var op expected) { \
     if (tst_report == TST_REPORT_FULL) \
-      printf ("(Rank:%d) Failed in " func_string " " #var " " #op " expected:%d but got:%d\n", \
-              tst_global_rank, expected, var); \
+      printf ("(Rank:%d) Failed in " func_string " (Line:%d) " #var " " #op " expected:%d but got:%d\n", \
+              tst_global_rank, __LINE__, expected, var); \
     tst_test_recordfailure (env); \
   }
 
@@ -33,13 +33,18 @@ int tst_env_status_check_init (struct tst_env * env)
 
 int tst_env_status_check_run (struct tst_env * env)
 {
+  MPI_Comm comm;
   MPI_Status status;
+  MPI_Request request;
   int cancelled, get_count, get_elements;
+  int flag;
 
-  memset (&status, 0xFF, sizeof (status));
+  comm = tst_comm_getcomm (env->comm);
+
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
   status.MPI_ERROR = 4711;
 
-  MPI_CHECK (MPI_Recv (NULL, 0, MPI_CHAR, MPI_PROC_NULL, 4711, MPI_COMM_WORLD, &status));
+  MPI_CHECK (MPI_Recv (NULL, 0, MPI_CHAR, MPI_PROC_NULL, 4711, comm, &status));
 
   MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
   MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
@@ -58,6 +63,225 @@ int tst_env_status_check_run (struct tst_env * env)
   LOCAL_CHECK ("MPI_Recv", cancelled, !=, 0);
   LOCAL_CHECK ("MPI_Recv", get_count, !=, 0);
   LOCAL_CHECK ("MPI_Recv", get_elements, !=, 0);
+
+
+  /*
+   * Do the same with MPI_Irecv + MPI_Request_get_status, expecting to have request pass that through the status
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Irecv (NULL, 0, MPI_CHAR, MPI_PROC_NULL, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Request_get_status (request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  /*
+   * If interpreting MPI-1.2, sec. 3.2.5, p. 22 (changing status.MPI_ERROR for single-completion
+   * wait/test calls) in the same sense for MPI_Recv, status.MPI_ERROR should not be changed,
+   *
+   * Nevertheless, we are not that anal.
+   */
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status", status.MPI_SOURCE, !=, MPI_PROC_NULL);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status", get_elements, !=, 0);
+
+  MPI_CHECK (MPI_Request_free (&request));
+  LOCAL_CHECK ("MPI_Irecv / MPI_Request_get_status / MPI_Request_free", request, !=, MPI_REQUEST_NULL);
+
+
+  /*
+   * Do the same with MPI_Irecv + MPI_Test, expecting to have request pass that through the status
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Irecv (NULL, 0, MPI_CHAR, MPI_PROC_NULL, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Test (&request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  /*
+   * If interpreting MPI-1.2, sec. 3.2.5, p. 22 (changing status.MPI_ERROR for single-completion
+   * wait/test calls) in the same sense for MPI_Recv, status.MPI_ERROR should not be changed,
+   *
+   * Nevertheless, we are not that anal.
+   */
+  LOCAL_CHECK ("MPI_Irecv / MPI_Test", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Test", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Test", status.MPI_SOURCE, !=, MPI_PROC_NULL);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Test", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Irecv / MPI_Test", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Test", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Irecv / MPI_Test", get_elements, !=, 0);
+
+
+  /********************* Persistent communication (MPI_PROC_NULL) **********************************/
+  /********************* As these are never started, expect an empty status *******************************/
+  /*
+   * Do the same for MPI_Send_init using MPI_Request_get_status:
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Send_init (&flag, 1, MPI_INT, MPI_PROC_NULL, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Request_get_status (request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", status.MPI_SOURCE, !=, MPI_ANY_SOURCE);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", get_elements, !=, 0);
+
+  MPI_CHECK (MPI_Request_free (&request));
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status / MPI_Request_free", request, !=, MPI_REQUEST_NULL);
+
+
+  /*
+   * Do the same for MPI_Send_init using MPI_Test:
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Send_init (&flag, 1, MPI_INT, MPI_PROC_NULL, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Test (&request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  LOCAL_CHECK ("MPI_Send_init / MPI_Test", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Test", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Test", status.MPI_SOURCE, !=, MPI_ANY_SOURCE);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Test", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Send_init / MPI_Test", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Test", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Test", get_elements, !=, 0);
+
+
+
+  /*
+   * Do the same for MPI_Recv_init using MPI_Request_get_status:
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Recv_init (&flag, 1, MPI_INT, MPI_PROC_NULL, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Request_get_status (request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", status.MPI_SOURCE, !=, MPI_ANY_SOURCE);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", get_elements, !=, 0);
+
+  MPI_CHECK (MPI_Request_free (&request));
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status / MPI_Request_free", request, !=, MPI_REQUEST_NULL);
+
+
+  /*
+   * Do the same for MPI_Recv_init using MPI_Test:
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Recv_init (&flag, 1, MPI_INT, MPI_PROC_NULL, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Test (&request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Test", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Test", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Test", status.MPI_SOURCE, !=, MPI_ANY_SOURCE);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Test", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Test", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Test", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Test", get_elements, !=, 0);
+
+
+
+  /********************* Persistent communication (specific source/dest) **********************************/
+  /*
+   * Do the same for MPI_Send_init with SPECIFIC destination (0):
+   * Have MPI_Request_get_status on this inactive request, but here with a specific source / destination
+   * Persisent, INactive Request, expect an empty status.
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Send_init (&flag, 1, MPI_INT, 0, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Request_get_status (request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", status.MPI_SOURCE, !=, MPI_ANY_SOURCE);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status", get_elements, !=, 0);
+
+  MPI_CHECK (MPI_Request_free (&request));
+  LOCAL_CHECK ("MPI_Send_init / MPI_Request_get_status / MPI_Request_free", request, !=, MPI_REQUEST_NULL);
+
+
+  /*
+   * Do the same for MPI_Recv_init with SPECIFIC destination (0):
+   * Have MPI_Request_get_status on this inactive request
+   */
+  memset (&status, DEFAULT_INIT_BYTE, sizeof (status));
+  status.MPI_ERROR = 4711;
+
+  MPI_CHECK (MPI_Recv_init (&flag, 1, MPI_INT, 0, 4711, comm, &request));
+
+  MPI_CHECK (MPI_Request_get_status (request, &flag, &status));
+  MPI_CHECK (MPI_Test_cancelled (&status, &cancelled));
+  MPI_CHECK (MPI_Get_count (&status, MPI_CHAR, &get_count));
+  MPI_CHECK (MPI_Get_elements (&status, MPI_CHAR, &get_elements));
+
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", flag, !=, 1);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", (status.MPI_ERROR != MPI_SUCCESS) && status.MPI_ERROR, !=, 4711);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", status.MPI_SOURCE, !=, MPI_ANY_SOURCE);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", status.MPI_TAG, !=, MPI_ANY_TAG);
+
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", cancelled, !=, 0);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", get_count, !=, 0);
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status", get_elements, !=, 0);
+
+  MPI_CHECK (MPI_Request_free (&request));
+  LOCAL_CHECK ("MPI_Recv_init / MPI_Request_get_status (2) / MPI_Request_free", request, !=, MPI_REQUEST_NULL);
 
   return 0;
 }
