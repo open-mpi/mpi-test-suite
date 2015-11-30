3c563e83fba680d0f0e5d92ed37e178cbef569d6
Gilles Gouaillardet <gilles@rist.or.jp>
2015-11-30 11:06:46 +0900

    mpi_test_suite: MPI version is now 3.1, so handle this correctly

--- env/tst_env_get_version.c
+++ env/tst_env_get_version.c
@@ -32,7 +32,7 @@ int tst_env_get_version_run (struct tst_env * env)
   MPI_CHECK (MPI_Get_version (&version, &subversion));
   tst_output_printf (DEBUG_LOG, TST_REPORT_MAX, "(Rank:%d) version: %d, subversion: %d\n", tst_global_rank, version, subversion);
 
-  int valid_mpi_versions[][2] = { {3,0}, {2,2}, {2,1}, {2,0}, {1,2} };
+  int valid_mpi_versions[][2] = { {3,1}, {3,0}, {2,2}, {2,1}, {2,0}, {1,2} };
   int i;
   for( i = 0; i < sizeof(valid_mpi_versions); i++) {
     if( valid_mpi_versions[i][0] == version && valid_mpi_versions[i][1] == subversion) {
